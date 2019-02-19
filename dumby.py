20fb35072acd08bcdb08f9b813f18fccf3b83410
Hana Hasan <hahasan@stu.naperville203.org>
2019-02-19 12:23:16 -0600

    added validation

--- dumby.py
+++ dumby.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+
+from . import app
+from .forms import EmailPasswordForm 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('DumbyPage.html')
+
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
 
 if __name__=="__main__":
     app.run(debug=True, host="0.0.0.0")
--- forms.py
+++ forms.py
@@ -1,9 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators = [DataRequired()])
-    password = PasswordField('Password', validators = [DataRequired()]);
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    firstName = StringField('firstName', validators = [DataRequired()])
+    lastName = PasswordField('lastName', validators = [DataRequired()])
