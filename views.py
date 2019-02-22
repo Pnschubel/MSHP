ef1f1744552f4f2c3f75b8af069ba6e4972d9aa8
Hana Hasan <hahasan@stu.naperville203.org>
2019-02-22 11:36:20 -0600

    We are restartinggit status

--- views.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template. redirect. url_for
-from . import app
-from .forms import EmailPasswordForm
-
-@app.route('/login', methods=["GET","POST"])
-def login():
-    form = EmailPasswordForm()
-    if form.validate_on_submit();
-    
