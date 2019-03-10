4bc96d5c800de495f9f835509226797fcfaab0ef
hanahasan <31442052+hanahasan@users.noreply.github.com>
2019-03-10 17:05:47 -0500

    Attempted to catch up
    remade app.py, login.html and results.html based off the tutorial. Wanna test out the files bc idk how to at home tbh

---
+++ frontEndForms/pythonFiles/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+##cmment
+@app.route('/')
+def form():
+    return render_template('login.html')
+
+@app.route('/result', methods = ['POST','GET'])
+def result():
+    if request.method == 'POST':
+        result = request.form ##gets form from form()??
+        return render_template("result.html", result = result)
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
