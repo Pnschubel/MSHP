1bc4b7bcff2299d3a188a2236cd754fe3099083f
Sean Kudrna <spkudrna@stu.naperville203.org>
2019-03-14 12:22:37 -0500

    Almost done with getters

--- MasterCopy/app/EZ_Funct.py
+++ MasterCopy/app/EZ_Funct.py
@@ -4,12 +4,108 @@
 import database
 from flask import current_app, g
 
+#Get Customer Info
+#-----------------
 def getCustomerName(email):
                         #This may have to be changed to .query_db
-    refPoint = customers.query.filter_by(customerEmail= email)
+    refPoint = customers.query.filter_by(customerEmail = email)
+    #refPoint = customer email
+
     return (refPoint.customerName)
+    #Return the name associated with customer email
 
 def getCustomerEmail(name):
                         #This may have to be changed to .query_db
     refPoint = customers.query.filter_by(customerName = name)
+    #refPoint = customer name
+
     return (refPoint.customerEmail)
+    #Retrun the email associated with the customer name
+
+
+
+
+#Get Car Info
+#-------------
+def getMake(email):
+                        #This may have to be changed to .query_db
+    refPoint = customers.query.filter_by(customerEmail = email)
+    #refPoint = customer email
+
+    transferID = refPoint.customerID
+    #transferID = customer ID number
+
+                        #This may have to be changed to .query_db
+    vehicle_refPoint = vehicles.query.filter_by(transferID)
+    #vehicle_refPoint = customer ID number attached to vehicles table
+
+    return(vehicle_refPoint.make)
+    #Return the make associated with customerID number
+
+def getModle(email):
+                        #This may have to be changed to .query_db
+    refPoint = customers.query.filter_by(customerEmail = email)
+    #refPoint = customer email
+
+    transferID = refPoint.customerID
+    #transferID = customer ID number
+
+                        #This may have to be changed to .query_db
+    vehicle_refPoint = vehicles.query.filter_by(transferID)
+    #vehicle_refPoint = customer ID number attached to vehicles table
+
+    return(vehicle_refPoint.model)
+    #Return the model associated with customerID number
+
+def getYear(email):
+                        #This may have to be changed to .query_db
+    refPoint = customers.query.filter_by(customerEmail = email)
+    #refPoint = customer email
+
+    transferID = refPoint.customerID
+    #transferID = customer ID number
+
+                        #This may have to be changed to .query_db
+    vehicle_refPoint = vehicles.query.filter_by(transferID)
+    #vehicle_refPoint = customer ID number attached to vehicles table
+
+    return(vehicle_refPoint.year)
+    #Return the year associated with customerID number
+
+
+
+
+#Get Repair Info
+#--------------
+def getRepairType(email):
+                        #This may have to be changed to .query_db
+    refPoint = customers.query.filter_by(customerEmail = email)
+    #refPoint = customer email
+
+    transferID = refPoint.customerID
+    #transferID = customer ID number
+
+                        #This may have to be changed to .query_db
+    vehicle_refPoint = vehicles.query.filter_by(transferID)
+    #vehicle_refPoint = customer ID number attached to vehicles table
+
+    transferID_Layer2 = vehicle_refPoint.vehicleID
+    #transferID_Layer2 = vehicleID number
+
+    repair_refPoint = repairs.query.filter_by(transferID_Layer2)
+    #repair_refPoint = vehicle ID number attached to repairs table
+
+    return(repair_refPoint.repairType)
+
+    
+
+
+
+
+
+
+
+    
+
+
+    
--- MasterCopy/app/schema.sql
+++ MasterCopy/app/schema.sql
@@ -18,12 +18,14 @@ CREATE TABLE customers(
     customerId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     customerName TEXT NOT NULL,
     customerEmail TEXT NOT NULL
+    customerPhoneNum INT 
     );
 
 CREATE TABLE vehicles(
     vehicleId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     make TEXT,
     model TEXT,
+    year INT,
     vin TEXT CHECK(vin is null or length(vin) == 14),
     customerId INT NOT NULL,
     FOREIGN KEY(customerId) REFERENCES customers(customerId)
@@ -32,6 +34,7 @@ CREATE TABLE vehicles(
 CREATE TABLE repairs(
     repairId INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     repairType TEXT NOT NULL,
+    repairDescription TEXT
     accepted BOOLEAN,
     vehicleId INT NOT NULL,
     FOREIGN KEY(vehicleId) REFERENCES vehicles(vehicleId)
