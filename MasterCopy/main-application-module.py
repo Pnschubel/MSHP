2e2581c6f19c0b52df75ede8359cfb83f7d20c29
Sean Kudrna <spkudrna@stu.naperville203.org>
2019-04-04 12:11:07 -0500

    Removed extraneous files that would mess with merging, reorganized files that would mess with merging. EZ_Funct End of Sprint 3

--- MasterCopy/main-application-module.py
+++
@@ -1,5 +0,0 @@
-from app import myApp
-
-if __name == "__main__":
-    app = create_app()
-    app.run(debug = True)
--- READMEdatabase.txt
+++
@@ -1,1 +0,0 @@
-This is a test branch for testing database concepts and implementation.
