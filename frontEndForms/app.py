098943eb6a1676439191629035505f0b92a7cfb5
hanahasan <hahasan@stu.naperville203.org>
2019-04-05 13:52:26 -0500

    validation sucks

--- frontEndForms/app.py
+++ frontEndForms/app.py
@@ -11,7 +11,7 @@ def form():
 @app.route('/result', methods = ['POST','GET'])
 def result():
     if request.method == 'POST':
-        result = request.form.to_dict() ##gets form from form()??
+        result = request.form.to_dict() ##get into a dictionary
         for field,userinput in result.items(): 
             print(field, userinput)
             return render_template("result.html", result = result)
@@ -20,37 +20,19 @@ def result():
 @app.route('/result', methods = ['POST','GET' ])
 def testResult():
     if request.method == 'POST':
+        error = None
         result = request.form.to_dict()
+        ##the validation!:(
 
-        error = "There is an error"
+        ##see if data is entered into required fields
+        if result[customerName] == 0 and result[customerEmail] == 1 and result[repairType] == 1:
 
-        for field, userinput in result.items():
-            ##checks email
-            if field == 'email':
-                if hasData(userInput) == 0 or emailChecker(userInput) == 0:
-                    ##email is wrong
-                    error = error + "Email is invalid, "
-            ##check vim 
-            if field == 'vinNumber':
-                if vinNumber(userInput) == 0 and hasData(userInput):
-                    ##vin number is wrong
-                    error = error + "Vin Number is invalid, "
-            if field == 'customerName' and field == 'model' and field == 'make' and field == 'repairType' and field == "repairDescription":
-                if hasData(userInput) == 0: 
-                    ##name, model, make, repairType, and decription is wrong
-                    error = error + "required fields were not filled in, "
-            if field == 'year': 
-                if hasData(userInput) == 0 and isintance(userInput, (int)):
-                    ##year is wrong
-                    error = error + "Year is incorrect: "
-                    return render_template("errorPage.html", result = result)
- 
-    else:
-        print (field, userinput)
+            ##require repairDescription if repair type is othe
+            if result[repairType] == "other":
+                error = error + "
 
-        flash(error)
+        
 
-        return render_template("errorPage.html", result = result)
-
+           
 if __name__ == '__main__':
     app.run(debug = True)
