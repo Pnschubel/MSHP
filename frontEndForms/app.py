dc4e4c3be1ab0a663dd2d093455220d4d0acf270
hanahasan <hahasan@stu.naperville203.org>
2019-04-05 13:02:40 -0500

    attempt to flash error

--- frontEndForms/app.py
+++ frontEndForms/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 import validation
 
 
@@ -16,28 +16,41 @@ def result():
             print(field, userinput)
             return render_template("result.html", result = result)
 
-##@app.route('/result', methods = ['POST','GET' ])
-##def testResult():
-##    if request.method == 'POST':
-##        result = request.form.to_dict()
-##        for field, userinput in result.items():
-##            ##checks email
-##            if field == 'email':
-##                if hasData(userInput) == 0 or emailChecker(userInput) == 0:
-##                    ##email is wrong
-##            ##check vim 
-##            if field == 'vinNumber':
-##                if vinNumber(userInput) == 0 and hasData(userInput):
-##                    ##vin number is wrong
-##            if field == 'customerName' and field == 'model' and field == 'make' and field == 'repairType' and field == "repairDescription":
-##if hasData(userInput) == 0: 
-##                    ##name, model, make, repairType, and decription is wrong
-##            if field == 'year': 
-##                if hasData(userInput) == 0 and isintance(userInput, (int)):
-##                    ##year is wrong
-##    else:
-##        print (field, userinput)
-##        return render_template("errorPage.html", result = result)
-##
+
+@app.route('/result', methods = ['POST','GET' ])
+def testResult():
+    if request.method == 'POST':
+        result = request.form.to_dict()
+
+        error = "There is an error"
+
+        for field, userinput in result.items():
+            ##checks email
+            if field == 'email':
+                if hasData(userInput) == 0 or emailChecker(userInput) == 0:
+                    ##email is wrong
+                    error = error + "Email is invalid, "
+            ##check vim 
+            if field == 'vinNumber':
+                if vinNumber(userInput) == 0 and hasData(userInput):
+                    ##vin number is wrong
+                    error = error + "Vin Number is invalid, "
+            if field == 'customerName' and field == 'model' and field == 'make' and field == 'repairType' and field == "repairDescription":
+                if hasData(userInput) == 0: 
+                    ##name, model, make, repairType, and decription is wrong
+                    error = error + "required fields were not filled in, "
+            if field == 'year': 
+                if hasData(userInput) == 0 and isintance(userInput, (int)):
+                    ##year is wrong
+                    error = error + "Year is incorrect: "
+                    return render_template("errorPage.html", result = result)
+ 
+    else:
+        print (field, userinput)
+
+        flash(error)
+
+        return render_template("errorPage.html", result = result)
+
 if __name__ == '__main__':
     app.run(debug = True)
