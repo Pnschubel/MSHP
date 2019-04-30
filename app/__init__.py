98f9f425bc90e0e2a15aec8cbf585a61533015d4
hanahasan <hahasan@stu.naperville203.org>
2019-04-30 12:21:09 -0500

    messages work, validation doesn't

--- app/__init__.py
+++ app/__init__.py
@@ -7,18 +7,10 @@ from flask import Flask
 def create_app(test_config=None):
     
     myApp = Flask(__name__, instance_relative_config=True)
-    
-<<<<<<< HEAD
-    #SECRET_KEY is to keep nasty hackers from doing dum stuff.
-    myApp.config['SECRET_KEY'] = 'comicSansBeepBoopBoopPotato'
-    #DATABASE is path of our database
-    myApp.config.from_mapping(SECRET_KEY="dev", DATABASE = os.path.join(myApp.instance_path, "CarDB.sqlite"))
 
-=======
     # SECRET_KEY is to keep nasty hackers from doing dum stuff.
     # DATABASE is path of our database
     myApp.config.from_mapping(SECRET_KEY="dev", DATABASE=os.path.join(myApp.instance_path, "CarDB.sqlite"))
->>>>>>> origin/master
 
     if test_config is None:
         # If there is a configured instance, load it. config.py can store values
@@ -33,20 +25,10 @@ def create_app(test_config=None):
     except OSError:
         pass
     
-<<<<<<< HEAD
-    #import your blueprints here
-    from myApp import database ##MOVED INSTANCE INTO APP AND CHANGED THIS FROM (FROM . IMPORT DATABASE TO IMPORT DATABASE) DUE TO RELATIVE FILE RETREVIAL
-    #from . import dummyTester
-    
-    #register your blueprints here.
-    #Can use flask tutorial
-    #Ex: myApp.register_blueprint(index.bp)
-=======
     # import your blueprints here
     from app import database
     from app import index
     import dummyTester
->>>>>>> origin/master
     
     # register your blueprints here.
     # Can use flask tutorial
@@ -57,13 +39,8 @@ def create_app(test_config=None):
     
     return myApp
 
-<<<<<<< HEAD
 
 #Justas's version of this file is signficantly more complicated
 #and probably better so if a) we're getting a lot of errors we can't
 #explain or b) we have the time, we should maybe go back to that.
-=======
-# Justas's version of this file is significantly more complicated
-# and probably better so if a) we're getting a lot of errors we can't
-# explain or b) we have the time, we should maybe go back to that.
->>>>>>> origin/master
+
--- app/app.py
+++ app/app.py
@@ -1,19 +1,20 @@
 from flask import Flask, render_template, request, flash
-from app import validation, create_app
+import validation 
+##from app import create_app
 
 app = Flask(__name__)
 
-create_app()
-
+##create_app()
+app.config['SECRET_KEY'] = 'hanas password'
 
 @app.route('/')
 def form():
-    return render_template('login.html')
+    return render_templater('login.html')
 
 @app.route('/result', methods = ['POST','GET' ])
 def result():
     if request.method == 'POST':
-        error = None
+        error = ""
         result = request.form.to_dict()
         print (result)
         print ("hello")
@@ -22,21 +23,22 @@ def result():
         
         if ok == "True":
             ##if required fields arent filled out
-            if validation.hasData(result['customerName']) == 0 and validation.hasData(result['customerEmail'])  == 0 and validation.hasData(result['repairType'])  == 0 and validation.hasData(result[repairType]) == 0:
-                error = error + "required data /n"
+            if validation.hasData(result['customerName']) == 0 and validation.hasData(result['customerEmail'])  == 0 and validation.hasData(result['repairType'])  == 0 and validation.hasData(result['repairType']) == 0:
+                error = error + "required data"
+                print("required fields")
                 ok = "False"
 
             ##require repairDescription if repair type is other
             if result['repairType'] == "other":
                 if validation.hasData(result['repairDescription']) == 0: 
-                    error = error + "repair description /n"
+                    error = error + "repair description"
                     ok = "False"
 
             ##require vin if year,make,model is null
             if validation.hasData(result['make']) == 0 and validation.hasData(result['model']) == 0 and validation.hasData(result['year']) == 0:
                 ##if vin is empty incorrect
                 if validation.hasData(result['vin']) == 0:
-                    error = error + "vehicle vin or vehicle make,model and year /n"
+                    error = error + "vehicle vin or vehicle make,model and year"
                     ok = "False"
                 else:
                     ## if vin number is entered incorrectly
@@ -49,18 +51,18 @@ def result():
                 
                 ##check make
                 if validation.hasData(result['make']) == 0:
-                    error = error + "Vehicle make is required /n"
+                    error = error + "Vehicle make is required"
                     ok = "False" 
                 if validation.hasData(result['model']) == 0: 
-                    error = error + "Vehicle model is required /n"
+                    error = error + "Vehicle model is required"
                     ok = "False" 
                 if validation.hasData(result['year']) == 0: 
-                        error = error + "Vehicle year is required /n"
+                        error = error + "Vehicle year is required"
                         ok = "False" 
 
             ##check if the email is correct
             if validation.hasData(result['customerEmail']) == 0 and validation.emailChecker(result['customerEmail']) == 0:
-                    error = error + "Email is invalid /n"
+                    error = error + "Email is invalid"
 
 
             
@@ -68,6 +70,7 @@ def result():
             ##see if ok variable is true false - see if you flash error or confirmation message:
             if ok == "False":
                 flash("Must include: "+ error)
+                print (:
             else:
                 flash("Your form was successfully submitted!") 
 
