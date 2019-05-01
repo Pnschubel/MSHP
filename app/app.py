cf3561ae8a40fde2ac9324d3801fa69b077a0dbd
hanahasan <hahasan@stu.naperville203.org>
2019-05-01 12:50:09 -0500

    flashed messages are in a loop

--- app/app.py
+++ app/app.py
@@ -14,36 +14,46 @@ def form():
 @app.route('/result', methods = ['POST','GET' ])
 def result():
     if request.method == 'POST':
-        error = ""
         result = request.form.to_dict()
-        print (result)
-        print ("hello")
+       
         ##the validation!:(
         ok = "True"
         
         if ok == "True":
             ##if required fields arent filled out
-            if validation.hasData(result['customerName']) == 0 and validation.hasData(result['customerEmail'])  == 0 and validation.hasData(result['repairType'])  == 0 and validation.hasData(result['repairType']) == 0:
-                error = error + "required data"
-                print("required fields")
+            if validation.hasData(result['customerName']) == 0:
+                print("required name")
+                flash("required name")
+
+                ok = "False"
+
+            if  validation.hasData(result['customerEmail'])  == 0:
+                print("required email")
+                flash("required email")
+                ok = "False"
+
+            if validation.hasData(result['repairType'])  == 0:
+                print("required fields aint filled out")
+                flash("required repairType")
                 ok = "False"
 
             ##require repairDescription if repair type is other
             if result['repairType'] == "other":
                 if validation.hasData(result['repairDescription']) == 0: 
-                    error = error + "repair description"
+                    print("require repair description")
+                    flash("required email")
                     ok = "False"
 
             ##require vin if year,make,model is null
             if validation.hasData(result['make']) == 0 and validation.hasData(result['model']) == 0 and validation.hasData(result['year']) == 0:
                 ##if vin is empty incorrect
                 if validation.hasData(result['vin']) == 0:
-                    error = error + "vehicle vin or vehicle make,model and year"
+                    flash("vehicle vin or vehicle make,model and year")
                     ok = "False"
                 else:
                     ## if vin number is entered incorrectly
                     if validation.vinNumber(result['vin']) == 0: 
-                        error = error + "vin number is incorrect"
+                        flash("vin number is incorrect")
                         ok = "False"
 
             ##check year,make,model if vn is empty
@@ -51,30 +61,23 @@ def result():
                 
                 ##check make
                 if validation.hasData(result['make']) == 0:
-                    error = error + "Vehicle make is required"
+                    flash("Vehicle make is required")
                     ok = "False" 
                 if validation.hasData(result['model']) == 0: 
-                    error = error + "Vehicle model is required"
+                    flash("Vehicle model is required")
                     ok = "False" 
                 if validation.hasData(result['year']) == 0: 
-                        error = error + "Vehicle year is required"
+                        flash("Vehicle year is required")
                         ok = "False" 
 
             ##check if the email is correct
             if validation.hasData(result['customerEmail']) == 0 and validation.emailChecker(result['customerEmail']) == 0:
-                    error = error + "Email is invalid"
+                    flash("Email is invalid")
+                    print ("this email is WRONG")
 
 
             
-
-            ##see if ok variable is true false - see if you flash error or confirmation message:
-            if ok == "False":
-                flash("Must include: "+ error)
-                print (:
-            else:
-                flash("Your form was successfully submitted!") 
-
-    return render_template("login.html",error=error)
+    return render_template("login.html")
   
 if __name__ == '__main__':
     app.run(debug = True)
