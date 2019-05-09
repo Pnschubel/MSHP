18cf736dc75222c7d00461b2d086d52d809c5dbb
Matias Bonta <bontamatias@gmail.com>
2019-05-09 11:39:21 -0500

    Added publish, renamed file

--- app/toolkit.py
+++ app/toolkit.py
@@ -278,3 +278,26 @@ def RemoveCustomer(cusID):
  
     query_db("DELETE FROM customers WHERE customerId = ?", (int(cusID),))
     return "Customer has been deleted"
+
+
+
+
+#PUBLISH FORM DATA TO THE DATABASE
+def publish(formInfo):
+
+   #Creates customer from validated form info
+   createCustomer(formInfo['customerName'], 
+                  formInfo['customerEmail'],
+                  formInfo.get('customerPhoneNum'))
+
+   #Creates vehicle form validated from info
+   createVehicle(formInfo.get('make'),
+                 formInfo.get('model'),
+                 formInfo.get('year'),
+                 formInfo.get('vin'))
+
+   #Creates repair from validated form info
+   createRepair(formInfo['repairType'],
+                formInfo['repairDescription'],
+                accepted = False,
+                completed = False)
