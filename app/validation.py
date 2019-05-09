2942de2b474a78e6b7e63c13de5ea9397ae97ef6
Matias Bonta <bontamatias@gmail.com>
2019-05-09 10:03:18 -0500

    Made file format more consistent

--- app/validation.py
+++ app/validation.py
@@ -1,4 +1,5 @@
 import re
+
 def hasData(userInput):
     if len(userInput) == 0:
         return 0
@@ -11,17 +12,12 @@ def vinNumber(userInput):
         return 1
     else:
         return 0
+    
 def emailChecker(userInput):
     re.match("[^@]+@[^\.]+\.[a-z]{2,3}",userInput)is not None
      
-
 def checkLength(stringLength, userInput):
     if userInput == stringLength:
         return 1
     else:
         return 0
-
-
-
-
-
